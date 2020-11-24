cff10bc85643bc509d6a544ad692ab0ff5851f96
Nikola Maeda <nmaeda@sfu.ca>
2020-11-24 16:39:23 +0900

    Added inputs

--- main.c
+++ main.c
@@ -11,13 +11,39 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
     char *printed_json = NULL;
     int minify, require_termination, formatted, buffered;
 
-    if(size == 0) return 0;
+
+    if(size <= offset) return 0;
+    if(data[size-1] != '\0') return 0;
+    if(data[0] != '1' && data[0] != '0') return 0;
+    if(data[1] != '1' && data[1] != '0') return 0;
+    if(data[2] != '1' && data[2] != '0') return 0;
+    if(data[3] != '1' && data[3] != '0') return 0;
+
+    minify              = data[0] == '1' ? 1 : 0;
+    require_termination = data[1] == '1' ? 1 : 0;
+    formatted           = data[2] == '1' ? 1 : 0;
+    buffered            = data[3] == '1' ? 1 : 0;
 
     json = cJSON_ParseWithOpts((const char*)data + offset, NULL, require_termination);
 
     if(json == NULL) return 0;
 
-    
+    if(buffered)
+    {
+        printed_json = cJSON_PrintBuffered(json, 1, formatted);
+    }
+    else
+    {
+        /* unbuffered printing */
+        if(formatted)
+        {
+            printed_json = cJSON_Print(json);
+        }
+        else
+        {
+            printed_json = cJSON_PrintUnformatted(json);
+        }
+    }
 
     if(printed_json != NULL) free(printed_json);
 
---
+++ run.sh
@@ -0,0 +1,6 @@
+mkdir build
+cd build
+cmake ..
+make
+
+./fuzz ../input -only_ascii=1 -print_final_stats=1
