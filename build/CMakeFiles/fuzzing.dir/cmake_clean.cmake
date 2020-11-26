ab527be043f761ab08513fea417ff5f977e1b1ef
Nikola Maeda <nmaeda@sfu.ca>
2020-11-25 22:18:46 -0800

    Final Results

---
+++ build/CMakeFiles/fuzzing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fuzzing.dir/main.c.o"
+  "fuzzing.pdb"
+  "fuzzing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fuzzing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
